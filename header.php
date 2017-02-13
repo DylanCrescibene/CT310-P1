2bc5697d88736d99f0944f2ec2deedbd93c73bc0
Brendon Powley <bpowley@sacramento.cs.colostate.edu>
2017-02-13 15:29:25 -0700

    centered elements in header

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
 echo '<div class="header">
-			<div class="jumbotron">
+			<div class="jumbotron" style="text-align:center">
 				<h1>CT310 Grocery Website</h1>
 				<img src="" class="img-circle" alt="grocery logo" width="50" height="50">
 			</div>
