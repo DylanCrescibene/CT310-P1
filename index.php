5d58bda2273561b6b02db939056d8b10deac0891
Dylan Crescibene <dcresc@richmond.cs.colostate.edu>
2017-02-13 14:15:45 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Ingredients For You</title>
+	<meta name="author" content="Dylan Crescibene and Brendon Powley">
+	<meta name="description" content="CT310 P1: Team 15">
+	<meta name="keywords" content="Dylan,Crescibene,Brendon,Powley,CSU,CT310,Colostate">
+	<link rel="stylesheet"
+		href="./style.css"
+		type="text/css" />
+</head>
+
+<body class="document">
+	<div class="mainContainer">
+		<p>Green Bean, Oregano, Mint</p>
+	</div>
+</body>
+
+</html>
