4e8f5703febf4efbb77ab9f06c88a340f3a1842d
Brendon Powley <bpowley@sacramento.cs.colostate.edu>
2017-02-13 15:09:00 -0700

    updated header.php

---
+++ header.php
@@ -0,0 +1,8 @@
+<?php
+echo '<div class="header">
+			<div class="jumbotron">
+				<h1>CT310 Grocery Website</h1>
+				<img src="" class="img-circle" alt="grocery logo" width="280" height="290" float="right">
+			</div>
+		</div>'
+?>
