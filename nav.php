c93684f1885eefab85e56eaf383c54a4348dccda
Brendon Powley <bpowley@sacramento.cs.colostate.edu>
2017-02-13 15:46:27 -0700

    updated nav.php

---
+++ nav.php
@@ -0,0 +1,16 @@
+<?php
+echo '<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="https://www.cs.colostate.edu/~ct310/yr2017sp/">CT310</a>					
+				</div>
+				<ul class="nav navbar-nav">
+					<li class="active"?><a href="./index.php">Home</a></li>
+					<li><a href="./about.php">About Us</a></li>
+					<li><a href="./mint.php">Mint</a></li>
+					<li><a href="./greenbean.php">Green Bean</a></li>
+					<li><a href="./oregano.php">Oregano</a></li>
+				</ul>
+			</div>
+		</nav>'
+?>
