c60d0484d9a6eff317d919748263c67efecfb331
Brendon Powley <bpowley@bugatti.cs.colostate.edu>
2017-02-24 15:28:53 -0700

    working

--- about.php
+++ about.php
@@ -21,15 +21,15 @@ include ('header.php');
 	<div class="row">
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
 			<h2>Dylan Crescibene</h2>
+			<img src="./dylan.jpg" class="img-circle" alt="dc" width="280" height="290">
 			<p>
-				<img src="./dylan.jpg" class="img-circle" alt="dc" width="280" height="290">
 				Dylan is a senior at CSU studying computer science. His interests include videogames, skateboarding, and music.
 			</p>
 		</div>
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
 			<h2>Brendon Powley</h2>
-			<p style="text-align: center">
-				<img src="./brendon.jpg" class="img-circle" alt="bp" width="280" height="290">
+			<img src="./brendon.jpg" class="img-circle" alt="bp" width="280" height="290">
+			<p>
 				Brendon is a junior at CST studying applied computing technology. his interests include sports, music, and biking.
 			</p>
 		</div>
