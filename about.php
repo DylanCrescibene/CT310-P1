dac81418a81fd61449803b984b0ad718d440f8bb
Brendon Powley <bpowley@olympia.cs.colostate.edu>
2017-02-24 09:51:13 -0700

    did a little work

--- about.php
+++ about.php
@@ -9,10 +9,14 @@ include ('header.php');
 
 <!-- Start contents of main page here. -->
 
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h2>About</h2>
+		</div>	
+	</div>"
+</div>
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	<h2>About</h2>
-</div>	
 
 
 
