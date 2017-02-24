12abdc676d7159de6f1ec9d65727b91f473d46c7
bpowley <brendon.powley@gmail.com>
2017-02-24 10:11:26 -0700

    I added some bootstrap rows to our about page

--- about.php
+++ about.php
@@ -12,9 +12,26 @@ include ('header.php');
 <div class="container">
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-			<h2>About</h2>
-		</div>	
+			<h1>About</h1>
+			<h3>
+				This is our Project 1 Page for CT310
+			</h3>
+		</div>
 	</div>"
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+			<h2>Dylan Crescibene</h2>
+			<p>
+				Dylan is a senior at CSU studying computer science. His interests include videogames, skateboarding, and music.
+			</p>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+			<h2>Brendon Powley</h2>
+			<p>
+				Brendon is a junior at CST studying applied computing technology. his interests include sports, music, and biking.
+			</p>
+		</div>
+	</div>
 </div>
 
 
