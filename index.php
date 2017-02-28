6df27467a0f73223f612dc2faccb50e801703905
bpowley <brendon.powley@gmail.com>
2017-02-27 19:54:03 -0700

    basic information on the homepage

--- index.php
+++ index.php
@@ -12,6 +12,17 @@ include ('header.php');
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 	<h2>Home Page</h2>
+
+	<p>
+		Welcome to our CT310 project 1 page. <br>
+		Here you will find examples of html, css styling, php, bootstrap, user authentication, and other dope web programming stuff.<br>
+		You will also find brief information on the following ingredients:
+		<ul>
+			<li>Green Beans</li>
+			<li>Mint</li>
+			<li>Oregano</li>
+		</ul>
+	</p>
 </div>
 
 
