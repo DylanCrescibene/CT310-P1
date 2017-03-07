553f3af5a526c72ff8ed3113ff8e23ec6bc57fa8
brendon powley <brendon.powley@gmail.com>
2017-03-06 23:16:10 -0700

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,10 +1,14 @@
 
+
+<?php if(isset($_SESSION["loggedIn"])){ ?>
+
 <div class="comment-box rounded">
   <div class="title-box">
     <h4>Comment Box</h4>
   </div>
   <div class="comment-section">
     <p>Leave a Comment!</p>
+    <?php echo "<p>logged in as [" .$_SESSION["sessionUser"] . "]</p>"?>
   </div>
   <form class="form-inline" role="form">
     <div class="form-group">
@@ -14,4 +18,10 @@
       <button class="btn btn-defualt">Add Comment</button>
     </div>
   </form>
+  
 </div>
+
+<?php }else { ?>
+  <p style="text-align: center"> <small>[You must log in to leave a comment] </small></p>
+
+<?php } ?>
